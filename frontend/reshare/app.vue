58125c5ab8e99eaac28d03b4907945ad1df0a4f3
uni-dos <uni.dos12@outlook.com>
2025-03-19 12:25:52 -0700

    added themes and a toolbar

--- frontend/reshare/app.vue
+++ frontend/reshare/app.vue
@@ -1,5 +1,27 @@
 <template>
+  <Toolbar style="border-radius: 3rem">
+    <template #start>
+        <div class="flex items-center gap-2">
+            <svg width="35" height="40" viewBox="0 0 35 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="..." fill="var(--p-text-color)" />
+                <path d="..." fill="transparent" />
+            </svg>
+            <Button label="Files" text plain />
+            <Button label="Edit" text plain />
+            <Button label="View" text plain />
+        </div>
+    </template>
+
+    <template #end>
+        <div class="flex items-center gap-2">
+            <Button label="Share" severity="contrast" size="small" />
+            <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" style="width: 32px; height: 32px" />
+        </div>
+    </template>
+</Toolbar>
   <div>
-   <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
   </div>
 </template>
--- frontend/reshare/nuxt.config.ts
+++ frontend/reshare/nuxt.config.ts
@@ -1,9 +1,18 @@
 import { defineNuxtConfig } from "nuxt/config";
-
+import Aura from '@primeuix/themes/aura';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   title : "Reshare",
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@primevue/nuxt-module']
+  modules: ['@nuxt/image', '@primevue/nuxt-module'], 
+
+  primevue: {
+    options: {
+      ripple : true,
+      theme : {
+        preset: Aura,
+      }
+    }
+  }
 })
