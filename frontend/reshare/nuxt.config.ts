130a3fc83aaa50021c5f7f562559d44ceaa68f7d
uni-dos <uni.dos12@outlook.com>
2025-03-19 11:46:02 -0700

    add primevue

--- frontend/reshare/nuxt.config.ts
+++ frontend/reshare/nuxt.config.ts
@@ -1,7 +1,9 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   title : "Reshare",
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/image']
+  modules: ['@nuxt/image', '@primevue/nuxt-module']
 })
