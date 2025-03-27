8e278ea53382e04509362d20d37856b6806cc1d5
uni-dos <uni.dos12@outlook.com>
2025-03-27 10:26:05 -0700

    added some endpoints

--- backend/backend.py
+++ backend/backend.py
@@ -1,30 +1,72 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import mysql.connector
 import os
 
-# connect to DB
-db_connection = mysql.connector.connect(
-    host = os.get('DB_URL'),
-    user = os.get('DB_UNAME'),
-    password = os.get('DB_PSWD'),
-    database = os.get('DB_NAME')
+
+def getDBConnection() :
+    
+    return mysql.connector.connect(
+        host = os.get('DB_URL'),
+        user = os.get('DB_UNAME'),
+        password = os.get('DB_PSWD'),
+        database = os.get('DB_NAME')
 )
 
 app = Flask(__name__)
 
+# not sure if needed
 @app.route('/')
 def helloworld():
     return 'Hello World'
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
-    return True
+    # in here we add the user to the db
+    # hash the password and create a uid
+    # uid will be the index and will be part of the 
 
-@app.route('/signup')
+    # simple test for now
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username != '' or password != '' :
+        print('username is ' + username)
+        return jsonify({"message" : "Welcome"}), 200
+
+    # return 309 if user exists
+    return jsonify({"message" : "user already exists"}), 309
+
+@app.route('/signup', methods=['POST'])
 def signup():
+     # in here we add the user to the db
+    # hash the password and create a uid
+    # uid will be the index and will be part of the 
+
+    # simple test for now
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username != '' or password != '' :
+        print('username is ' + username)
+        return jsonify({"message" : "Welcome"}), 200
+
+    # return 309 if user exists
+    return jsonify({"message" : "user already exists"}), 309
+
+# get posts
+@app.route('/post',  methods=['GET'])
+def post() :
     return True
 
+# create post
+@app.route('/post',  methods=['POST'])
+def post() :
+    return True
+
+# delete post
+@app.route('/post',  methods=['DELETE'])
+def post() :
+    return True
 
 if __name__ == '__main__' :
     app.run()
-    db_connection.close()
