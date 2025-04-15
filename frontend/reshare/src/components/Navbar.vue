577456779326a1f2fd2c24d97e7e49702115f216
Arden Yan <148033490+ardenyan13@users.noreply.github.com>
2025-04-14 19:46:41 -0700

    Create basic navbar component

---
+++ frontend/reshare/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<script setup>
+</script>
+
+<template>
+    <div class="header">
+      <ul class="left-links">
+        <li><a href="#">Reshare</a></li>
+        <li><a href="#">Following</a></li>
+        <li><a href="#">Bookmarks</a></li>
+        <li><a href="#">Create</a></li>
+      </ul>
+      <ul class="right-links">
+        <li><a href="#">Profile</a></li>
+      </ul>
+    </div>
+</template>
+
+<style scoped>
+
+.header {
+  padding: 16px;
+  display: flex;
+  justify-content: space-between;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  gap: 16px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+</style>
--- frontend/reshare/src/pages/Homepage.vue
+++ frontend/reshare/src/pages/Homepage.vue
@@ -1,4 +1,9 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+</script>
+
 <template>
+    <Navbar />
     <!-- Might need to be the username -->
      <h1>{{ $route.params.username }}</h1>
 </template>
