791cf1650998474b753687df5d284a5592b549c8
Arden Yan <148033490+ardenyan13@users.noreply.github.com>
2025-04-16 15:37:53 -0700

    Add sign up password validation and display error (username taken)

--- frontend/reshare/src/pages/Signup.vue
+++ frontend/reshare/src/pages/Signup.vue
@@ -10,16 +10,24 @@
   
         <FloatLabel variant="on">
           <FormField label="Password">
-          <Password v-model="password" id="password" toggleMask required />
+          <Password v-model="password" id="password" :feedback="false" toggleMask required />
           <label for="password">Password</label>
           </FormField>
         </FloatLabel>
+
+        <FloatLabel variant="on">
+          <FormField label="Reenter">
+            <Password v-model="reenter" id="password" :feedback="false" toggleMask required />
+            <label for="confirmPassword">Re-enter Password</label>
+          </FormField>
+        </FloatLabel>
   
-        <Button label="Sign Up" type="submit" :disabled="isSubmitting" />
+        <Button label="Sign Up" type="submit" :disabled="isSubmitting"/>
       </form>
   
-      <div v-if="errorMessage">
-        <p>{{ errorMessage }}</p>
+      <div>
+        <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+        <p v-if="passwordValidationFailed" class="error">Passwords do not match!</p>
       </div>
     </div>
   </template>
@@ -32,33 +40,51 @@
       return {
         username: '',
         password: '',
+        reenter: '',
         isSubmitting: false,
-        errorMessage: ''
+        errorMessage: '',
+        passwordValidationFailed: false
       };
     },
     methods: {
       async handleSubmit() {
+        // reset validation
+        this.errorMessage = '';
+        this.passwordValidationFailed = false;
+
+        // validate password match
+        if (this.password !== this.reenter) {
+          this.passwordValidationFailed = true;
+          return;
+        }
+
         this.isSubmitting = true;
         
         // The fields can never be empty as primvue checks
         await axios.post('http://localhost:5000/signup', {
             "username": this.username,
             "password" : this.password
-          })
-          .then(response => {
-            console.log(response);
-            // pass in username
-            // so router.push(nextpage params: username)
-            if (response.status === 201) {
-              // move to the next page
-              this.$router.push('/' + response.data.username + '/homepage');
+        })
+        .then(response => {
+          console.log(response);
+          // pass in username
+          // so router.push(nextpage params: username)
+          if (response.status === 201) {
+            // move to the next page
+            this.$router.push('/' + response.data.username + '/homepage');
+          }
+        })
+        .catch(error => {
+          // retrieve the "taken" or "message field from backend response"
+          if (error.response && error.response.data && error.response.data.taken) {
+            this.errorMessage = error.response.data.message;
+          }
+          else {
+            this.errorMessage = "Could not signup. Try again later";
             }
-          })
-          .catch(error => {
-            // error.response.data
-            console.log(error.response.data.message);
-          });
-          this.isSubmitting = false;
+          console.log(error.response.data.message);
+        });
+        this.isSubmitting = false;
           
         
       }
@@ -70,6 +96,7 @@
   
   .container {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vh;
@@ -79,6 +106,10 @@
     flex-direction: column;   
     gap: 10px;
     text-align: center;   
+  }
+
+  .error {
+    color: red;
   }
   
   
