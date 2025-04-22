48c3a45ab4336173059092668973d174b0f7bb2b
uni-dos <uni.dos12@outlook.com>
2025-04-22 10:06:00 -0700

    cards render

--- frontend/reshare/src/pages/Homepage.vue
+++ frontend/reshare/src/pages/Homepage.vue
@@ -3,23 +3,47 @@
     <!-- Might need to be the username -->
      <h1>{{ $route.params.username }}</h1>
 
+    <div>
+        <Recipe v-for="r in items" 
+        :key="r.recipe_id" :title="r.title"
+        :user="r.author_username" :steps="r.steps" 
+        :rating="r.sustainability_rating" :servings="r.servings"/>
+        </div>
 </template>
 
 <script lang="ts">
     import axios from 'axios'
+    
+    interface Recipe_Result {
+        recipe_id : Number,
+        title : String,
+        short_description : String,
+        steps : String,
+        time : String,
+        servings : Number,
+        author_username : String,
+        sustainability_rating : Number,
+        average_rating : Number
+    }
     export default {
+        data () {
+           return{ items : <Recipe_Result[]>[] }
+        },
         async mounted() {
-            await axios.get('http://localhost:5000/posts')
+            await axios.get('http://localhost:5000/recipes')
             .then(response => {
-                if (response.status === 201) {
+                
+                if (response.status === 200) {
                     // we got at least 1 post
+                    console.log("Mounted")
+                    this.items = response.data;
                 }
                 else if (response.status === 303) {
                     // no posts
                 }
             })
             .catch(error => {
-
+                console.log(error);
             });
         },
     }
