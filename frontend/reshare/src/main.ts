c5a5e9838f8776d3d0816aaeb721fcc0159bfbdb
Arden Yan <148033490+ardenyan13@users.noreply.github.com>
2025-04-23 14:30:55 -0700

    Set up create recipe page

--- frontend/reshare/src/main.ts
+++ frontend/reshare/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura';
 import { FormField } from '@primevue/forms';
-import { Button, InputText, Message, Password, FloatLabel, Card, Divider, ProgressSpinner} from 'primevue';
+import { Button, InputText, Message, Password, FloatLabel, Card, Divider, ProgressSpinner, Textarea, InputNumber, InputIcon, IconField, Select } from 'primevue';
 
 // for icons 
 import 'primeicons/primeicons.css'
@@ -20,6 +20,7 @@ import Signup from './pages/Signup.vue';
 import Login from './pages/Login.vue';
 import MainPage from './pages/MainPage.vue'
 import Homepage from './pages/Homepage.vue'
+import CreateRecipe from './pages/CreateRecipe.vue'
 
 // this will change the current page
 const routes = [
@@ -27,6 +28,7 @@ const routes = [
     {path: '/login', component: Login},
     {path: '/signup', component: Signup},
     {path: '/:username/homepage', component: Homepage},
+    {path: '/:username/create', component: CreateRecipe}
 ];
 
 const router = createRouter({
@@ -55,4 +57,9 @@ app.component('Card', Card);
 app.component('Divider', Divider);
 app.component('Recipe', Recipe);
 app.component('ProgressSpinner', ProgressSpinner);
+app.component('Textarea', Textarea)
+app.component('InputNumber', InputNumber)
+app.component('InputIcon', InputIcon)
+app.component('IconField', IconField)
+app.component('Select', Select)
 app.mount('#app');
