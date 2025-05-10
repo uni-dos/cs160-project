60a9995046e1bd8f777bf39e2c2f7c2503c3bb22
Arden Yan <148033490+ardenyan13@users.noreply.github.com>
2025-05-09 20:05:01 -0700

    Add a search input to navbar

--- frontend/reshare/src/components/Navbar.vue
+++ frontend/reshare/src/components/Navbar.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="header">
       <ul class="left-links">
-        <Button @click="emitClicked('logo')"><IcOrganicFood/><label>Rehare</label></Button>
+        <Button @click="emitClicked('logo')"><IcOrganicFood/><label>Reshare</label></Button>
         <Button label="Following" icon="pi pi-users" @click="emitClicked('following')"></Button>
         <Button label="Bookmarks" icon="pi pi-bookmark" @click="emitClicked('bookmarks')"></Button>
         <Button label="Create" icon="pi pi-pen-to-square" @click="emitClicked('create')"></Button>
+        <div class="search-container">
+          <IconField>
+            <InputIcon class="pi pi-search" />
+            <InputText v-model="searchQuery" variant="filled" size="large" placeholder="Search" @keyup.enter="handleSearch" />
+        </IconField>
+        </div>
       </ul>
       <ul class="right-links">
         <SplitButton icon="pi pi-user" @click="emitClicked('profile')" :model="logout"><label>{{ $route.params.username }}</label></SplitButton>
@@ -17,7 +23,9 @@
 import axios from 'axios'
 export default {
   data () {
-    return {logout : [{label : "Logout", command: async () => {
+    return {
+      searchQuery: '',
+      logout : [{label : "Logout", command: async () => {
       await axios.post('/logout')
       .then (response => {
         if (response.status === 200) {
@@ -37,6 +45,10 @@ export default {
         this.$router.push('homepage');
       }
     },
+    handleSearch() {
+      console.log("Searching for: ", this.searchQuery);
+      this.searchQuery = '';
+    }
   },
   props : {
     user : {
@@ -61,6 +73,20 @@ ul {
   padding: 0;
   display: flex;
   gap: 16px;
+}
+
+.search-container {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.search-container input {
+  padding: 8px;
+  font-size: 16px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  width: 200px; /* Adjust width as needed */
 }
 
 
