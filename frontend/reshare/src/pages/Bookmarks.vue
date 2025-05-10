1d72310aa4196e1f71f48401cf138e920d649807
Arden Yan <148033490+ardenyan13@users.noreply.github.com>
2025-05-10 11:51:39 -0700

    Finish bookmarks page

--- frontend/reshare/src/pages/Bookmarks.vue
+++ frontend/reshare/src/pages/Bookmarks.vue
@@ -1,10 +1,51 @@
 <template>
+    <Navbar />
 
+    <h2>Your bookmarked recipes:</h2>
+
+    <div class="row">
+        <Recipe v-for="r in items" 
+            :key="r.recipe_id" :title="r.title"
+            :user="r.author_username" :steps="r.steps" :desc="r.short_description" 
+            :rating="r.sustainability_rating" :servings="r.servings" :time="r.time" :average_rating="r.average_rating" :ingredients="r.ingredients"/>
+    </div>
 </template>
 <script lang="ts">
+    import axios from 'axios'
+    import { type Recipe_Result } from '../data/Ingredient'
     export default {
+        data () {
+            return {
+                items : <Recipe_Result[]>[] 
+            }
+        },
+        methods: {
+            async fetchBookmarks() {
+                await axios.get(`/get-bookmarks/${this.$route.params.username}`)
+                .then(response => {
+                    if (response.status === 200) {
+                        this.items = response.data.bookmarks;
+                    }
+                    else {
+                        this.items = [];
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.$router.push('/login');
+                })
+            }
+        },
+        mounted() {
+            this.fetchBookmarks();
+        }
     }
 </script>
 
-<style>
+<style scoped>
+.row {
+    display: flex;
+    grid-auto-flow: row;
+    gap: 10px;
+}
 </style>
