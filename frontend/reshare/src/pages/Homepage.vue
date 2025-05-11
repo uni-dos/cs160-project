968cd6a84f05cdb50dcaf05ee153db31da226563
Arden Yan <148033490+ardenyan13@users.noreply.github.com>
2025-05-10 22:42:08 -0700

    Adjust css for recipe card alignment

--- frontend/reshare/src/pages/Homepage.vue
+++ frontend/reshare/src/pages/Homepage.vue
@@ -3,7 +3,7 @@
 
     <div class="row">
         <Recipe v-for="r in items" 
-            :key="r.recipe_id" :title="r.title" :recipeId="r.recipe_id"
+            :key="r.recipe_id" class="recipe-card" :title="r.title" :recipeId="r.recipe_id"
             :user="r.author_username" :steps="r.steps" :desc="r.short_description" 
             :rating="r.sustainability_rating" :servings="r.servings" :time="r.time" :average_rating="r.average_rating" :ingredients="r.ingredients"/>
         
@@ -43,7 +43,20 @@
 <style scoped>
 .row {
     display: flex;
-    grid-auto-flow: row;
-    gap: 10px;
+    flex-wrap: wrap;
+    align-items: stretch;
+    justify-content: center;
+    gap: 20px;
+}
+
+.recipe-card {
+    flex: 1 1 300px;
+    max-width: 300px;
+    min-width: 250px;
+    overflow: hidden;
+    scrollbar-width: none;
+    outline: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 8px;
+    padding: 5px;
 }
 </style>
